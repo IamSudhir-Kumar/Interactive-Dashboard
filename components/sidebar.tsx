460151a090fd4d40d05e6adb0d6703c70fa47f6e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-04-07 16:34:21 +0530

    Refactor mobile sidebar component and add new sidebar item component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,30 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+
 type Props = {
-    className?: string;
-}
+  className?: string;
+};
 export const Sidebar = ({ className }: Props) => {
-    return (
-        <div className={cn("flex bg-[#F875AA] h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col", className)}>
-            Sidebar
+  return (
+    <div
+      className={cn(
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/blue.png" height={40} width={40} alt="mascot" />
+          <h1 className="text-2xl font-extrabold text-blue-600 tracking-wide">
+            Polygot Pro
+          </h1>
         </div>
-    )
-}
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem />
+      </div>
+    </div>
+  );
+};
